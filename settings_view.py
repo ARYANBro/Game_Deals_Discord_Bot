1801c859a5c41af3b074c7e042a1253d3ba4068f
ARYANBro <nalinbro28@gmail.com>
2023-06-27 15:05:05 +0530

    fixed _filter_updates_defaults()

--- settings_view.py
+++ settings_view.py
@@ -7,47 +7,35 @@ class SettingsView(discord.ui.View):
     only_aaa = False
     on_sale = False
 
-    _select_options1 = []
-    _select_options2 = []
-    
+    _select_options = []
+
     for i, store in enumerate(sales_finder.game_sales.get_stores()):
-        if i == 25:
-            break
-        _select_options1.append(
+        _select_options.append(
             discord.SelectOption(
                 label=store['storeName'],
                 value=store['storeID'],
                 emoji='🛒',
             )
         )
-        
-    for store in sales_finder.game_sales.get_stores()[25:]:
-        _select_options2.append(
-            discord.SelectOption(
-                label=store['storeName'],
-                value=store['storeID'],
-                emoji='🛒',
-            )
-        )
-    
+
     def __init__(self):
         super().__init__(timeout=None)
 
-    @discord.ui.select(placeholder='Filter options 1', min_values=1, max_values=len(_select_options1), options=_select_options1, custom_id='sw_filter_select_01')
+    @discord.ui.select(placeholder='Filter options 1', min_values=1, max_values=len(_select_options[:25]), options=_select_options[:25], custom_id='sw_filter_select_01')
     async def filter_select(self, interaction: discord.Interaction, select: discord.ui.Select):
         if select.values:
             SettingsView.filtered_stores.extend(select.values)
 
-        self._filter_update_defaults(select.values, self._select_options1)
-        await interaction.response.defer()
+        self._filter_update_defaults(select.values)
+        await interaction.response.edit_message(view=self)
 
-    @discord.ui.select(placeholder='Filter options 2', max_values=len(_select_options2), options=_select_options2, custom_id='sw_filter_select_02')
+    @discord.ui.select(placeholder='Filter options 2', max_values=len(_select_options[25:]), options=_select_options[25:], custom_id='sw_filter_select_02')
     async def filter_select2(self, interaction: discord.Interaction, select: discord.ui.Select):
         if select.values:
             SettingsView.filtered_stores.extend(select.values)
             
-        self._filter_update_defaults(select.values, self._select_options2)
-        await interaction.response.defer()    
+        self._filter_update_defaults(select.values)
+        await interaction.response.edit_message(view=self)
 
     @discord.ui.select(placeholder='Sort By', options=[
         discord.SelectOption(label='Deal Rating', value='Deal Raiting'),
@@ -82,7 +70,7 @@ class SettingsView(discord.ui.View):
     @discord.ui.button(label='Reset filter', style=discord.ButtonStyle.red, custom_id='sw_b_reset_filter')
     async def reset_filter(self, interaction: discord.Interaction, button: discord.ui.Button):
         SettingsView.filtered_stores = []
-        for select_option in self._select_options1:
+        for select_option in self._select_options:
             select_option.default = False
         await interaction.response.edit_message(view=self)
 
@@ -94,21 +82,21 @@ class SettingsView(discord.ui.View):
         SettingsView.on_sale = not SettingsView.on_sale
         await interaction.response.edit_message(view=self)
 
-    def _filter_update_defaults(self, selected_values, select_options):
-        # ! Buggy, for some reason some values are removed when a button is pressed
-        selected_values = sorted(selected_values)
+    def _filter_update_defaults(self, selected_values):
+        selected_values = sorted(selected_values, key=int)
 
+        # We only want to reset the values of the select options that are not selected but those are also seperated by two selection lists
+        select_options = []
+        if int(selected_values[0]) > 25:
+            select_options = self._select_options[25:]
+        else:
+            select_options = self._select_options[:25]
+
+        # Reset all values
         for select_option in select_options:
             select_option.default = False
 
-        search_idx = 0
-        for select_option in select_options:
-
-            if search_idx == len(selected_values):
-                break
-
-            for value in selected_values[search_idx:]:
-                if value == select_option.value:
-                    select_option.default = True
-                    search_idx += 1
-                    break
+        # The selected values are the index of the select options + 1
+        for value in selected_values:
+            index = int(value) - 1
+            self._select_options[index].default = True
